e04b460ac819c8ea3e817b6c0d301a209434e1d5
igardus <igardus@gmail.com>
2025-04-15 16:33:38 +0300

    Update: use dynamic Amazon Linux 2 AMI and simplify variables

--- main.tf
+++ main.tf
@@ -2,12 +2,27 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Automatically fetch the latest Amazon Linux 2 AMI (x86_64)
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
 
   tags = {
     Name = "TerraformWebServer"
   }
 }
-
--- variables.tf
+++ variables.tf
@@ -1,16 +1,7 @@
 variable "aws_region" {
-  description = "The AWS region to deploy to"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "ami_id" {
-  description = "AMI ID for the instance"
-  type        = string
+  default = "us-east-1"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
+  default = "t2.micro"
 }

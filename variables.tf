eb79d1ca013408000690f298b046814359f319c2
igardus <igardus@gmail.com>
2025-04-15 15:56:44 +0300

    Initial Terraform AWS webserver project

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
